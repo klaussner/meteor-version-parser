b5ea5c9acf2d1ed1f68f05cc7ad47015bc486cbd
Christian Klaussner <developer@klaussner.me>
2017-07-15 11:06:43 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget \
+  https://raw.githubusercontent.com/meteor/meteor/46fddec786e3c8561ac9963fdb54ee3c9f0ddeea/packages/package-version-parser/package-version-parser.js \
+  -q -O package-version-parser.js
